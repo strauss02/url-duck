2d78c3260059c2e9ba89401ebcfc8611f57c4b57
Ido Strauss <strauss02@hotmail.co.il>
2021-11-03 20:50:01 +0200

    add basic server functions

--- server/app.js
+++ server/app.js
@@ -1,2 +1,14 @@
 const express = require('express')
 const app = new express()
+const cors = require('cors')
+const path = require('path')
+
+app.use(cors())
+
+app.use('/', express.static(path.resolve('../public')))
+
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve('../public/index.html'))
+})
+
+app.listen(process.env.PORT || 3000, () => console.log('Server is running...'))
